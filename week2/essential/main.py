eaa5bcc66ba1144fb3255d766d2448baff31e7d5
ycs-202007034 <heesup9683@naver.com>
2025-03-13 20:40:00 +0900

    오류 수정

--- week2/essential/main.py
+++ week2/essential/main.py
@@ -1,27 +1,36 @@
 import csv
 
-log_file = "essential/mission_computer_main.log"  # 경로 변경
-error_file = "essential/critical_issues.log"  # 경로 변경
+log_file = "essential/mission_computer_main.log" 
+error_file = "essential/critical_issues.log" 
 
-logs = []
+logs1 = []
+logs2 = []
+
+print("Hello Mars")
 
 with open(log_file, "r", encoding="utf-8") as file:
     reader = csv.reader(file)
     next(reader)  # 헤더 건너뛰기
     for row in reader:
-        logs.append(row)
+        logs1.append(row)
 
-# 로그를 시간 역순으로 정렬
-logs.sort(reverse=True, key=lambda x: x[0])
+with open(log_file, "r", encoding="utf-8") as file:
+    reader = csv.reader(file)
+    next(reader)  # 헤더 건너뛰기
+    for row in reader:
+        logs2.append(row)
 
+logs2.sort(reverse=True, key=lambda x: x[0])
+
+print("=== 로그 (시간 정순) ===")
+for log in logs1:
+    print(",".join(log))
 print("=== 로그 (시간 역순) ===")
-for log in logs:
+for log in logs2:
     print(",".join(log))
 
-# 특정 키워드가 포함된 중요 로그 필터링
-critical_logs = [log for log in logs if "Oxygen tank" in log[2] or "explosion" in log[2]]
+critical_logs = [log for log in logs2 if "Oxygen tank" in log[2] or "explosion" in log[2] or "powered down" in log[2]]
 
-# 필터링된 로그를 새로운 파일에 저장
 with open(error_file, "w", encoding="utf-8") as file:
     writer = csv.writer(file)
     writer.writerow(["timestamp", "event", "message"])
